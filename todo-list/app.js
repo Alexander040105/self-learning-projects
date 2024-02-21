9d7c42a993e083b44e4d0dab377a4e811aec79ef
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-02-21 22:09:40 +0800

    natapos ko somehow yung basic functions

--- todo-list/app.js
+++ todo-list/app.js
@@ -2,15 +2,24 @@ const addTask = document.querySelector('#addTask');
 const completedTask = document.querySelector('#completedTask');
 const taskInput = document.querySelector('#taskInput');
 const taskTable = document.querySelector('#taskTable');
+let li;
 
 addTask.onclick = appendTask;
+completedTask.onclick = delTask;
 
 function appendTask() {
     var userInput = taskInput.value;
     console.log(userInput);
-    const li = document.createElement('li');
+    li = document.createElement('li');
     const node = document.createTextNode(userInput);
     li.appendChild(node);
-
     taskTable.appendChild(li);
+    taskInput.value = '';
+}
+
+function delTask() {
+    const selectedLi = taskTable.querySelector('li');
+    if (selectedLi) {
+        taskTable.removeChild(selectedLi);
+    }
 }
