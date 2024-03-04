ef75f753077b4ab24a9e91137569736d91466ed4
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-03-04 20:24:13 +0800

    medyo naayos ko na somehow yung paggamit nung api

--- pokemon_api/app.js
+++ pokemon_api/app.js
@@ -1,100 +1,83 @@
-// fetch("https://pokeapi.co/api/v2/pokemon/gyarados")
-// .then(response =>{
-//     if(!response.ok){
-//         throw new Error("Could not fetch resource");
-//     }
-//     return response.json();
-// })
-// .then(data => console.log(data))
-// .catch(error => console.error(error));
-
-
-async function fetchData(){
-    try{
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-
-        if(!response.ok){
-            throw new Error("Could not fetch resource");
-        }
-        const data = await response.json();
-        console.log(data);
-
-        const pokemonSprite = data.sprites.front_default;
-        const pokeSprite = document.getElementById("pokemonSprite");
-
-        pokeSprite.src = pokemonSprite;
-        pokeSprite.style.display = "block";
-    }
-    catch(error){
-        console.error(error)
-    }
-}
-
-
-const pokemon = ["charizard","pikachu", "reshiram", "mewtwo"]
+const pokemon = ["charizard", "pikachu", "mewtwo", "zekrom", "reshiram"];
 let currentIndex = 0;
+const result = document.getElementById("result");
+const next = document.getElementById("next");
+const start = document.getElementById("start");
+const submit = document.getElementById("submit");
+const hint = document.getElementById("hint");
+const pokemonType = document.getElementById("pokemonType");
 
-async function questions(pokemons){
-    let currentPokemon = pokemon[currentIndex];
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemon}`);
+async function startPokemon(pokemonName) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
     if (!response.ok) {
-        throw new Error(`Could not fetch resource for ${pokemons[i]}`);
+        throw new Error(`Could not fetch resource for ${pokemonName}`);
     }
 
     const data = await response.json();
     const pokemonSprite = data.sprites.front_default;
+    console.log(data.name);
     const pokeSprite = document.getElementById("pokemonSprite");
-    const result = document.getElementById("result");
-    const next = document.getElementById("next");
-    const start = document.getElementById("start");
+
     pokeSprite.src = pokemonSprite;
     pokeSprite.style.display = "block";
     result.innerText = "";
     pokemonName.innerText = "";
+}
 
-    pokemonName.addEventListener("click", function(){
-        start.innerText = "Submit"
-    });
+function fetchNextPokemon() {
+    currentIndex++;
+    if (currentIndex < pokemon.length) {
+        startPokemon(pokemon[currentIndex]);
+        next.style.display = "none"; 
+    } else {
+        result.innerText = "Congratulations! You've completed all Pokemon.";
+        next.style.display = "none"; 
+    }
+}
 
-    
+function submitPokemon() {
+    let currentPokemon = pokemon[currentIndex];
     if (pokemonName.value.toLowerCase() === currentPokemon) {
         result.innerText = "You are Correct";
+        pokemonName.value = "" ;
+        pokemonType.innerText = "" ;
         next.style.display = "flex";
 
-        next.addEventListener("click", function(){
-            currentIndex++;
-            result.innerText = "";
-            pokemonName.value = "" ;
-            questions(pokemon);
-        });
-    } else if (pokemon.value !== currentPokemon){
+        next.removeEventListener("click", fetchNextPokemon); 
+        next.addEventListener("click", fetchNextPokemon);
+
+        // Additional check for the last Pokemon
+        if (currentIndex === pokemon.length - 1) {
+            next.style.display = "none";
+        }
+    } else {
         result.innerText = "You are incorrect bro";
         pokemonName.value = "";
     }
 }
 
-// async function questions(pokemons) {
-//     for (let i = 0; i < pokemons.length; i++) {
-//         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemons[i]}`);
+// Initial call to start fetching Pokemon
+startPokemon(pokemon[currentIndex]);
 
-//         if (!response.ok) {
-//             throw new Error(`Could not fetch resource for ${pokemons[i]}`);
-//         }
 
-//         const data = await response.json();
-//         const pokemonSprite = data.sprites.front_default;
-//         const pokeSprite = document.getElementById("pokemonSprite");
-//         const result = document.getElementById("result");
-//         pokeSprite.src = pokemonSprite;
-//         pokeSprite.style.display = "block";
+async function hintPokemon(){
+    let currentPokemon = pokemon[currentIndex];
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemon}`);
+    const data = await response.json();
+    
+    const pokemonData1 = data.types[0].type.name;
+    const pokemonData2 = data.types[1].type.name;
+    console.log(data.types.length)
+    if(data.types.length === 2){
+        pokemonType.innerText = "The type of this pokemon is " + pokemonData1 + " & " + pokemonData2;
+    } else{
+        pokemonType.innerText = "The type of this pokemon is " + pokemonData1
+    }
+}
 
-//         if (pokemonName.value.toLowerCase() === pokemons[i]) {
-//             result.innerText = "You are Correct"
-//         }
-//     }
-// }
+
+
 
 
 
