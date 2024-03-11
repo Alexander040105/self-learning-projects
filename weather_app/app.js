3a98f177d57089d4da86ba1d1f8bfab373c4aece
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-03-11 21:56:41 +0800

    3/11/2024 GUMAWA NG SIMPLENG WEATHER APP

---
+++ weather_app/app.js
@@ -0,0 +1,57 @@
+const search = document.getElementById("search");
+const countryName = document.getElementById("countryName");
+const countryHeader = document.getElementById("country");
+const weatherIcon = document.getElementById("weatherIcon");
+const temperature = document.getElementById("temperature");
+const weatherType = document.getElementById("typeWeather");
+const latitude = document.getElementById("latitude");
+const longitude = document.getElementById("longitude");
+const latLabel = document.getElementById("latLabel");
+const lonLabel = document.getElementById("lonLabel");
+const darkToggle = document.getElementById("darkMode");
+
+darkToggle.addEventListener("click", function darkMode(){
+    const body = document.body;
+    const isDarkMode = body.classList.toggle("dark-mode");
+    darkToggle.innerText = isDarkMode ? "light_mode" : "dark_mode";
+});
+
+
+
+async function weatherApi(){
+    const apiKey = "67b4cb11470a49dd817142325241003";
+    let country = countryName.value.toLowerCase();
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${country}`);
+    const forecastResponse = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${country}`);
+    // const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=philippines`);
+
+
+    const data = await response.json();
+    const dataForecast = await forecastResponse.json();
+    let currentData = data.current.condition.text;
+    let lat = data.location.lat;
+    let lon = data.location.lon;
+    let icon = data.current.condition.icon;
+    let temp = data.current.temp_c;
+    countryHeader.innerText = countryName.value.toUpperCase();
+    weatherType.innerText = currentData ;
+    temperature.innerText = temp + "°";
+    latitude.innerText = lat;
+    longitude.innerText = lon;
+    latLabel.style.display = "block"
+    weatherIcon.style.display = "block"
+    lonLabel.style.display = "block"
+    weatherIcon.src = icon;
+    if(!response.ok){
+        throw new Error('Could not fetch the data or resource');
+    }
+    countryName.value = "";
+    console.log(data);
+    console.log(dataForecast);
+    console.log(currentData);
+    console.log(countryName);
+    // console.log(data);
+}
+
+search.addEventListener("click", weatherApi);
+// weatherApi();
