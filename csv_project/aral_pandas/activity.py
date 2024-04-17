d71ef2593d89ce883251d4ac8415ed75ced82d4b
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-04-17 23:00:31 +0800

    may mmw activity akong ginawa

---
+++ csv_project/aral_pandas/activity.py
@@ -0,0 +1,31 @@
+# num1 = input("Enter the first Number")
+# num2 = input("Enter the second Number")
+# i = 0
+# for i in range(int(num1)):
+#     i += 1
+#     for j in range(int(num2)):
+#         j +=1
+#         print(i * j)
+
+# stringInput = input("Enter a string you want to reverse: ")
+# stringToReverse = []
+
+# for letter in stringInput:
+#     stringToReverse.append(letter)
+
+# stringToReverse.reverse()
+# print(''.join(stringToReverse)) 
+
+# num1 = input("Enter the first number in your pattern: ")
+# num2 = input("Enter the second number you want as your increment: ")
+# num3 = input("Enter the third number for the nth:")
+
+# num1 = 10
+# num2 = 2
+# num3 = 7
+
+# for element in range(int(num3)):
+#     num1 += int(num2)
+#     num2 += 2 
+#     print(num1)
+    
