b4c618c9febb6f7a3d193133e6159da7f38e6bdb
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-06-20 20:30:28 +0800

    6/20/2024 I tried to use subplots on the Air Quality API

--- flask_trial/app.py
+++ flask_trial/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
-
+import os
+import smtplib
+import csv
 #configure app
 app = Flask(__name__)
 
@@ -17,16 +19,21 @@ def index():
     #jinjaVariableName=pythonVariableName when using the second parameter for render_template
     return render_template("index.html")
 
-@app.route("/registrants")
-def registrants():
-    #the students jinja variable is accessed in the for loop of the html
+@app.route("/registered")
+def registered():
+    #putting the contents of the CSV file onto a List 
+    file = open("registered.csv", "r")
+    readCSV = csv.reader(file)
+    students = list(readCSV)
+    file.close()
+    # the students jinja variable is accessed in the for loop of the html
     return render_template("registered.html", students=students)
 
 
 @app.route("/register", methods=["POST"])
 def register():
     
-    #request.form.get() if it is from a input tags
+    #request.form.get() if it is from a the form
     name = request.form.get("name")
     dorm = request.form.get("dorm")
     
@@ -36,9 +43,17 @@ def register():
     if not name or not dorm:
         return render_template("fail.html")
     
-    #appends the name of the student and dorms since it passes the formatted string in the list
-    students.append(f"{name} from {dorm}")
-    return redirect("/registrants")
+    #adding the data from the site onto a csv file
+    file = open("registered.csv", "a")
+    writeCSV = csv.writer(file)
+    #writes the data onto a row in the csv
+    writeCSV.writerow((name, dorm))
+    file.close()
+    
+    return render_template("success.html")
+    # #appends the name of the student and dorms since it passes the formatted string in the list
+    # students.append(f"{name} from {dorm}")
+    # return redirect("/registrants")
 
 if __name__ == "__main__":
     app.run(debug=True)
