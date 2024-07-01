12efd2bba1d74b148daa5fc523105a4339bbdb45
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-07-01 13:33:36 +0800

    Tried the Tech with Tim Tutorial Again

--- flask_with_db/website/auth.py
+++ flask_with_db/website/auth.py
@@ -2,14 +2,14 @@ from flask import Blueprint, render_template
 #define this as a Blueprint of the site where all routes are in here
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return "login"
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "logout"
 
-@auth.route('/signup')
+@auth.route('/signup' , methods=['GET','POST'])
 def signup():
-    return "signup"
+    return render_template("sign_up.html")
