217bfb1998e48d09409f2c79298839c7e31ca678
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-07-14 16:13:45 +0800

    7/14/2024 started the projectttt

--- movie_api_project/app.py
+++ movie_api_project/app.py
@@ -3,18 +3,26 @@ import requests
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 
 API_KEY = os.getenv('API_KEY_OMDB')
 
 app = Flask(__name__)
 
-
-def getData():
-    api_url = ''
+@app.route("/search", methods=['POST'])
+def searchMovie():
+    userSearch = request.form.get('movieSearch')
+    processedData = userSearch.replace(' ', '+')
+    api_url = f'http://www.omdbapi.com/?apikey={API_KEY}&s={processedData}'
+    response = requests.get(api_url)
+    data = response.json()
+    movies = data.get('Search', []) 
+    
+    return render_template("display.html", movies=movies)
     
 @app.route("/")
 def index():
-    return "hello"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
