3e411109b414f9db182f8144d6f7aa4743bf28ab
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-07-26 21:09:16 +0800

    started integrating flask and gspread for the aircooles website and done implementing it for now

--- aircooles_website/app.py
+++ aircooles_website/app.py
@@ -1,16 +1,70 @@
-from flask import Flask, render_template, redirect, requests
+from flask import Flask, render_template, redirect, request
+import requests
+import csv
+import gspread
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import datetime
 
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+
+credential = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+client = gspread.authorize(credential)
+
+spreadsheet_id = "1Q_7ZcMpJeyHMhlhcFL3xvh3JpnSTZL12otyvzzZ9108"
+workbook = client.open_by_key(spreadsheet_id)
+
+sheet1 = workbook.worksheet("Sheet1")
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
-@app.route("/getDetails", methods=['POST'])
+@app.route('/', methods=['GET'])
+def index():
+    cell_display = sheet1.get_all_values()
+    cellList = list(cell_display)
+    # return render_template("index.html", billList=billList, nameList=nameList, headers=headers)
+    return render_template("index.html", cellList=cellList)
+
+@app.route("/getDetails", methods=['POST', 'GET'])
 def getDetails():
-    print()
+    #services availed
+    cleaning = request.form.get("cleaning")
+    check_up = request.form.get("check_up")
+    dismantle = request.form.get("dismantle")
+    relocation = request.form.get("relocation")
+    installation = request.form.get("installation")
     
+    #client details
+    clientBill = request.form.get("clientBill")
+    clientName = request.form.get("clientName")
+    clientAddress = request.form.get("clientAddress")
+    
+    client_bill_display = sheet1.col_values(2)
+    
+    empty_row = len(client_bill_display) + 1  # +1 for the next row
+    currentTime = datetime.datetime.now().strftime('%m/ %d/ %Y %X')
+    
+    #updates the customer details on the google spreadsheet
+    sheet1.update_cell(empty_row, 1, currentTime)
+    sheet1.update_cell(empty_row, 2, clientName)
+    sheet1.update_cell(empty_row, 3, clientBill)
+    sheet1.update_cell(empty_row, 4, clientAddress)
+    
+    sheet1.update_cell(empty_row, 5, cleaning)
+    sheet1.update_cell(empty_row, 6, check_up)
+    sheet1.update_cell(empty_row, 7, dismantle)
+    sheet1.update_cell(empty_row, 8, relocation)
+    sheet1.update_cell(empty_row, 9, installation)
+    
+    return redirect("/")
     
 if __name__ == '__main__':
     app.run(debug=True)
--- coral_island_profit_tracker/app.py
+++ coral_island_profit_tracker/app.py
@@ -1,5 +1,26 @@
 from flask import Flask, render_template, redirect, request
+import requests
 import csv
+import gspread
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+
+credential = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+client = gspread.authorize(credential)
+
+spreadsheet_id = "1Q_7ZcMpJeyHMhlhcFL3xvh3JpnSTZL12otyvzzZ9108"
+spreadsheet = client.open_by_key(spreadsheet_id)
+
+values = spreadsheet.sheet1.row_values(1)
+print(values)
 
 app = Flask(__name__)
 
@@ -7,10 +28,10 @@ app = Flask(__name__)
 def getProfit():
     dailyProfit = request.form.get("dailyProfit")
     
-    with open('file.csv', 'w', newline='') as file:
-        dailyProfit = int(dailyProfit)
-        writer = csv.writer(file)
-        writer.writerow([dailyProfit])
+    # with open('file.csv', 'w', newline='') as file:
+    #     dailyProfit = int(dailyProfit)
+    #     writer = csv.writer(file)
+    #     writer.writerow([dailyProfit])
     
     return render_template("index.html" , dailyProfit=dailyProfit)
 
