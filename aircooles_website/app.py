0382c9b5c5b9a1e379ff2e42138d43f44c48cdd7
Alexander Jon <alexanderjonsolis05@gmail.com>
2024-07-28 20:25:00 +0800

    Somehow finished the UI

--- aircooles_website/app.py
+++ aircooles_website/app.py
@@ -37,7 +37,7 @@ def index():
 def getDetails():
     #services availed
     cleaning = request.form.get("cleaning")
-    check_up = request.form.get("check_up")
+    check_up = request.form.get("check-up")
     dismantle = request.form.get("dismantle")
     relocation = request.form.get("relocation")
     installation = request.form.get("installation")
@@ -45,6 +45,7 @@ def getDetails():
     #client details
     clientBill = request.form.get("clientBill")
     clientName = request.form.get("clientName")
+    clientNumber = request.form.get("clientNumber")
     clientAddress = request.form.get("clientAddress")
     
     client_bill_display = sheet1.col_values(2)
@@ -56,13 +57,14 @@ def getDetails():
     sheet1.update_cell(empty_row, 1, currentTime)
     sheet1.update_cell(empty_row, 2, clientName)
     sheet1.update_cell(empty_row, 3, clientBill)
-    sheet1.update_cell(empty_row, 4, clientAddress)
+    sheet1.update_cell(empty_row, 4, clientNumber)
+    sheet1.update_cell(empty_row, 5, clientAddress)
     
-    sheet1.update_cell(empty_row, 5, cleaning)
-    sheet1.update_cell(empty_row, 6, check_up)
-    sheet1.update_cell(empty_row, 7, dismantle)
-    sheet1.update_cell(empty_row, 8, relocation)
-    sheet1.update_cell(empty_row, 9, installation)
+    sheet1.update_cell(empty_row, 6, cleaning)
+    sheet1.update_cell(empty_row, 7, check_up)
+    sheet1.update_cell(empty_row, 8, dismantle)
+    sheet1.update_cell(empty_row, 9, relocation)
+    sheet1.update_cell(empty_row, 10, installation)
     
     return redirect("/")
     
